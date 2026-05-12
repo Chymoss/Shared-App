120b8ca271a5a8c2c88658d2e895a994c658346b
Chymoss <dengzhaoguo1993@163.com>
2026-05-12 17:31:25 +0800

    APIkey

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from utils import generate
 st.title("🎦1分钟学习街霸6<视频脚本生成>")
 
 with st.sidebar:
-    style=st.text_input("请输入操作模式",type="password")
+    API_key=st.text_input("请输入API密钥",type="password")
     st.markdown("[SF6官方地址](https://www.streetfighter.com/6/zh-hant)")
 
 character = st.text_input("请输入查询人物")
+style = st.text_input("请输入操作模式")
 
 submit= st.button("生成攻略")
+
+if submit and not API_key:
+    st.info("请输入你的密钥（中文）")
 if submit and not style:
     st.info("请输入你的操作模式（中文）")
     st.stop()
@@ -18,6 +22,6 @@ if submit and not character:
     st.stop()
 if submit:
     with st.spinner(("AI正在胡编中:")):
-        response  = generate(character,style)
+        response  = generate(API_key,character,style)
     st.success("success")
     st.write(response)
--- utils.py
+++ utils.py
@@ -2,12 +2,12 @@ from langchain.chat_models import init_chat_model
 import os
 # deepseek用法
 from langchain_deepseek import ChatDeepSeek
-model = ChatDeepSeek(model="deepseek-chat", api_key=os.environ.get('DEEPSEEK_API_KEY'))
+#model = ChatDeepSeek(model="deepseek-chat", api_key=os.environ.get('DEEPSEEK_API_KEY'))
 from langchain_core.messages import HumanMessage
 
 
-def generate(character,style):
-
+def generate(API_key, character,style):
+    model = ChatDeepSeek(model="deepseek-chat", api_key=API_key)
     prompt = f"""
     请围绕街霸6游戏人物“{character}”
     生成一段150词以内的视频脚本，展示{style}操作模式下该角色基本连段和实战思路。
