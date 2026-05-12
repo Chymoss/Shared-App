613d87bc21076b66a4e299e29a8c9f6d733782f6
Chymoss <dengzhaoguo1993@163.com>
2026-05-12 17:16:11 +0800

    1
    1

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from utils import generate
+
+st.title("🎦1分钟学习街霸6<视频脚本生成>")
+
+with st.sidebar:
+    style=st.text_input("请输入操作模式",type="password")
+    st.markdown("[SF6官方地址](https://www.streetfighter.com/6/zh-hant)")
+
+character = st.text_input("请输入查询人物")
+
+submit= st.button("生成攻略")
+if submit and not style:
+    st.info("请输入你的操作模式（中文）")
+    st.stop()
+if submit and not character:
+    st.info("请输入你的人物（中文）")
+    st.stop()
+if submit:
+    with st.spinner(("AI正在胡编中:")):
+        response  = generate(character,style)
+    st.success("success")
+    st.write(response)
---
+++ utils.py
@@ -0,0 +1,28 @@
+from langchain.chat_models import init_chat_model
+import os
+# deepseek用法
+from langchain_deepseek import ChatDeepSeek
+model = ChatDeepSeek(model="deepseek-chat", api_key=os.environ.get('DEEPSEEK_API_KEY'))
+from langchain_core.messages import HumanMessage
+
+
+def generate(character,style):
+
+    prompt = f"""
+    请围绕街霸6游戏人物“{character}”
+    生成一段150词以内的视频脚本，展示{style}操作模式下该角色基本连段和实战思路。
+
+    要求：
+    1. 语言自然流畅
+    2. 适合短视频口播
+    3. 有吸引力
+    4. 控制在150词以内
+    """
+
+    response = model.invoke([
+        HumanMessage(content=prompt)
+    ])
+
+    return response.content
+
+#print(generate("玩街霸6游戏"))
